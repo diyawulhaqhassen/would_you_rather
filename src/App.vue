a75e6c324392272a9e48837ff3f2794cc2a8ef82
Diya <dddd>
2021-11-24 19:38:23 -0600

    Lab10 done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+  <h1>Would you rather....</h1>
+    <WouldYouRather v-bind:question="wyrQuestion"
+                    v-bind:answer1="wyrAnswer1"
+                    v-bind:answer2="wyrAnswer2"
+                    v-on:answer-changed="answerChanged"></WouldYouRather>
+
+    <p>{{ userSelectionMessage }}</p>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data(){
+    return {
+      wyrQuestion:'Would you rather live in a house shaped like a circle or a house shaped like a triangle?',
+      wyrAnswer1:'circle',
+      wyrAnswer2:'triangle',
+      userSelectionMessage:'',
+    }
+  },
+  methods:{
+    answerChanged(choice){
+      this.userSelectionMessage=`Thanks! You chose ${choice}`
+    }
   }
 }
 </script>
@@ -20,7 +41,12 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
   margin-top: 60px;
+  background: blueviolet;
+}
+
+p{
+  font-style: italic;
 }
 </style>
